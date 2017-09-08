a77ba76bcaa297d5519a151c0164cf7bfbea62a2
Kent <kent@linkwish.com>
2017-09-08 23:41:27 +0800

    log

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ handler = WebhookHandler('3a376ac7d336060b079f677c5f3b414c') #Your Channel Secre
 def callback():
     # get X-Line-Signature header value
 
-    p("flag1")
     signature = request.headers['X-Line-Signature']
     p("signature:"+signature);
 
@@ -42,10 +41,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    p("flag2");
+    
     text = event.message.text #message from user
     p("text:"+text);
     p("event.reply_token:"+event.reply_token);
+
+    #Line 系統token 不回應
+    if event.reply_token == '00000000000000000000000000000000'
+       return
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text)) #reply the same message from user
