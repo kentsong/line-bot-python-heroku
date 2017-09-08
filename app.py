8ec3c4bb99b694963766277624a655aba8b4e6b1
Kent <kent@linkwish.com>
2017-09-08 23:24:35 +0800

    print

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import sys
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -19,14 +21,18 @@ handler = WebhookHandler('3a376ac7d336060b079f677c5f3b414c') #Your Channel Secre
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    System.out.println("flag1");
+    print "flag1"
+    sys.stdout.flush()
+    p("1111111111")
+
     signature = request.headers['X-Line-Signature']
     System.out.println("signature:"+signature);
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    System.out.println("Request body: " + body);
+    
+    #System.out.println("Request body: " + body);
 
     # handle webhook body
     try:
@@ -38,14 +44,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-     System.out.println("flag2");
+    #System.out.println("flag2");
     text = event.message.text #message from user
-System.out.println("text:"+text);
-System.out.println("event.reply_token:"+event.reply_token);
+    #System.out.println("text:"+text);
+    #System.out.println("event.reply_token:"+event.reply_token);
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=text)) #reply the same message from user
     
+def p(*args):
+  print args[0] % (len(args) > 1 and args[1:] or [])
+  sys.stdout.flush()    
 
 import os
 if __name__ == "__main__":
